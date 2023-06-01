f86caa9b4bb2e38bc4af201dfe0707689c28b26c
Mo Sorkhpar <Sorkhpar@outlook.com>
2023-05-31 20:10:59 -0400

    Fix main commented codes

--- main.py
+++ main.py
@@ -48,23 +48,20 @@ def generate_png_from_latex(name, symbol: str, paths: dict[str, str], png_size):
 if __name__ == '__main__':
     load_dotenv()
     csv_input_file = os.path.abspath(os.getenv("INPUT_PATH", "./latex_symbols.csv"))
-    output_dir = os.getenv("INPUT_PATH", "output")
+    output_dir = os.path.abspath(os.getenv("INPUT_PATH", "./output"))
     png_size = int(os.getenv("PNG_SIZE", "1024"))
     paths = {
-        "tex": os.getenv("TEX_DIR_PATH", f"{output_dir}/tex"),
-        "dvi": os.getenv("DVI_DIR_PATH", f"{output_dir}/dvi"),
-        "svg": os.getenv("SVG_DIR_PATH", f"{output_dir}/svg"),
-        "png": os.getenv("PNG_DIR_PATH", f"{output_dir}/png"),
-        "png_t": os.getenv("PNG_TRANSPARENT_DIR_PATH", f"{output_dir}/png_transparent"),
-        "skeleton": os.getenv("SKELETON_DIR_PATH", f"{output_dir}/skeleton"),
+        "tex": os.path.abspath(os.getenv("TEX_DIR_PATH", f"{output_dir}/tex")),
+        "dvi": os.path.abspath(os.getenv("DVI_DIR_PATH", f"{output_dir}/dvi")),
+        "svg": os.path.abspath(os.getenv("SVG_DIR_PATH", f"{output_dir}/svg")),
+        "png": os.path.abspath(os.getenv("PNG_DIR_PATH", f"{output_dir}/png")),
+        "png_t": os.path.abspath(os.getenv("PNG_TRANSPARENT_DIR_PATH", f"{output_dir}/png_transparent")),
+        "skeleton": os.path.abspath(os.getenv("SKELETON_DIR_PATH", f"{output_dir}/skeleton")),
     }
     [os.makedirs(path, exist_ok=True) for name, path in paths.items()]
 
     # Read the input csv file
     df: DataFrame = pd.read_csv(csv_input_file)
-    print('''
-|Index|Name|LaTeX symbol| SVG | PNG | PNG | Skeleton | Tex | DVI |  
-|:--: |:--:|:--:        |:--: | :--:| :--: |:------: | :--:| :--:|''')
 
     for index, row in df.iterrows():
         name = row['Name']
@@ -72,14 +69,10 @@ if __name__ == '__main__':
         image = row['Image']
 
         # Generate png file
-        #generate_png_from_latex(name, symbol, paths, png_size)
+        generate_png_from_latex(name, symbol, paths, png_size)
 
-        svg_file = os.path.join(paths['svg'], f"{name}.svg")
-        png_transparent = os.path.join(paths['png_t'], f"{name}.png")
-        tex_file = os.path.join(paths['tex'], f"{name}.tex")
-        dvi_file = os.path.join(paths['dvi'], f"{name}.dvi")
-        skeleton_file = os.path.join(paths['skeleton'], f"{name}.png")
         png_file = os.path.join(paths['png'], f"{name}.png")
-        print(f"|{index+1}|{name}|{symbol}|<img src='{svg_file}' height='32' width='32'> | [link]({png_file}) | [link]({png_transparent}) | [link]({skeleton_file}) | [link]({tex_file}) | [link]({dvi_file}) | ")
+        skeleton_file = os.path.join(paths['skeleton'], f"{name}.png")
+
         # Generate skeleton of the generated file
-        #process_image(png_file, skeleton_file)
+        process_image(png_file, skeleton_file)
